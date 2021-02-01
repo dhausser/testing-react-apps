440558a85007b5ddf60f2c8775b85206131eb371
Davy Hausser <davy.hausser@gmail.com>
2021-02-01 21:25:19 +0100

    Mocking Modules: Act Function

--- src/__tests__/exercise/06.js
+++ src/__tests__/exercise/06.js
@@ -39,8 +39,10 @@ test('displays the users current location', async () => {
   render(<Location />)
   expect(screen.getByLabelText(/loading/i)).toBeInTheDocument()
 
-  resolve()
-  await promise
+  await act(async () => {
+    resolve()
+    await promise
+  })
 
   expect(screen.queryByLabelText(/loading/i)).not.toBeInTheDocument()
   expect(screen.getByText(/latitude/i)).toHaveTextContent(
